61cb748b6d766b27170dedbab089bdea2738a96c
MafsMachado <mafsmachado@users.noreply.github.com>
2022-07-15 10:28:41 +0100

    begin

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "header.php";
 <section id="intro" class="pt-5 pb-5">
     <div class="container mx-auto">
         <div class="row">
-            <div class="col-6">
+            <div class="col-6 left pr-1">
                     <div class="imgIntro text-center">
                         <img src="img/general/forYourHome.jpeg" alt="Main Home" width="400">
                         <div class="hover">
@@ -13,13 +13,19 @@ include "header.php";
                             <p class="text-white">Home</p>
                         </div>
                     </div>
-                    <h1 class="text-right pt-2">Everything for</h1>
-                    <h1 class="text-right">your home.</h1>
+                    <h1 class="pt-2">Everything for</h1>
+                    <h1 class="">your home.</h1>
             </div>
-            <div class="col-6">
-                <h1 class="text-left">Everything for</h1>
-                <h1 class="text-left pb-2">your baby.</h1>
-                <img src="img/general/forYourBaby.jpg" alt="Main Baby" width="400">
+            <div class="col-6 right pl-1">
+                <h1>Everything for</h1>
+                <h1 class="pb-2">your baby.</h1>
+                <div class="imgIntro text-center">
+                    <img src="img/general/forYourBaby.jpg" alt="Main Baby" width="400">
+                    <div class="hover">
+                            <div class="shade"></div>
+                            <p class="text-white">Home</p>
+                        </div>
+                </div>
             </div>
         </div>
     </div>

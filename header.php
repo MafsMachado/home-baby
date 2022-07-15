cfce2bdd83ff588b4f6936bc6bc1d5c19fe199ef
MafsMachado <107553904+MafsMachado@users.noreply.github.com>
2022-07-15 09:09:46 +0100

    begin

---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home & Baby</title>
+
+    <link rel="icon" href="img/logoicon.png" type="image/iconpng">
+    <link rel="stylesheet" href="inc/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="pt-2 pb-2">
+        <section class="logo">
+            <img src="img/fulllogo.png" alt="Full Logo" width="160">
+        </section>
+        <section class="navbar">
+            <div class="navIcons">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search mr-1" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                    <input type="text" name="search">
+                </div>
+            </div>
+            <div class="navLinks">
+                <ul>
+                    <li><a href="index.php" class="text-decoration-none">Homepage</a></li>
+                    <li><a href="home.php" class="text-decoration-none">Home</a></li>
+                    <li><a href="baby.php" class="text-decoration-none">Baby</a></li>
+                    <li><a href="aboutUs.php" class="text-decoration-none">About Us</a></li>
+                    <li><a href="brands.php" class="text-decoration-none">Brands</a></li>
+                    <li><a href="contact.php" class="text-decoration-none">Contact</a></li>
+                </ul>
+            </div>
+        </section>
+    </nav>

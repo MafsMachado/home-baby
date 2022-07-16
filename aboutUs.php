e66489540a39193fc1e7fec96d52a46123fe2f62
MafsMachado <107553904+MafsMachado@users.noreply.github.com>
2022-07-16 21:28:20 +0100

    refresh

--- aboutUs.php
+++ aboutUs.php
@@ -6,7 +6,7 @@ include "header.php";
     <div class="row mx-auto align-items-center">
         <div class="col-6">
             <h1>About Us</h1>
-            <p class="pt-3">Home & baby was born after my passion for decoration and searching for things for babies while being pregnant began. I decided to create this store to all the moms that want to do their babies bedroom, buy essentials and revamp their house, or just to the people that love or need the lovely things selected by me, the best quality, original designs.</p>
+            <p class="pt-3">Home & baby was born after while being pregnant and started searching for things for my baby, and never find cute and different things. I decided to create this store to all the moms that want to do their babies bedroom, buy the essentials and revamp their house, or just to the people that love or need the lovely things selected by me, with the best quality, and original designs.</p>
         </div>
         <div class="col-6 text-center">
             <img src="img/general/aboutUs.jpg" alt="About Us" width="400">
